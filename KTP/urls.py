1ba4bfd3bb0c117c3080dd981958d01c20e8bfe9
Brooklyn-xeus <ooosamstar551@gmail.com>
2026-04-16 13:48:23 +0530

    Add buses and users apps

--- KTP/urls.py
+++ KTP/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', lambda request: redirect('admin/')),   
     path('admin/', admin.site.urls),
     path('v1/', include('users.urls')),
     path('v1/', include('buses.urls')),
